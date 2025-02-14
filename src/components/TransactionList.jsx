3741f972a7ebaf9214cf0eb48a945d00e0a73a05
asrithalingala <alingala@hawk.iit.edu>
2025-02-14 00:04:40 -0600

    updated code

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 
 const TransactionList = () => {
