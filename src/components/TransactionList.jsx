7f9307e5b6f4a490248864a62b8a2d5de8212e25
asrithalingala <alingala@hawk.iit.edu>
2025-02-16 00:00:10 -0600

    changed

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -13,16 +13,17 @@ const TransactionList = () => {
 
   // Build a unique list of categories from transactions
   const categories = ["All", ...new Set(transactions.map((t) => t.category))];
+//return statement
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-4" >
       <h3 className="fw-bold">Transaction History</h3>
       <div className="mb-3">
         <label className="form-label">Filter by Category:</label>
         <select
           className="form-select"
           value={filter}
-          onChange={(e) => setFilter(e.target.value)}
+          onChange={(e) => setFilter(e.target)}
         >
           {categories.map((cat) => (
             <option key={cat} value={cat}>
